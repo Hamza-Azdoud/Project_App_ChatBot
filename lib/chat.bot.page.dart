3737a7e3bd4a55b27c81d89246a333238bf9d2c8
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-07-12 15:57:06 +0100

    Chat Bot page (V1)

--- lib/chat.bot.page.dart
+++ lib/chat.bot.page.dart
@@ -1,17 +1,95 @@
 import 'package:flutter/material.dart';
 
-class ChatBotPage extends StatelessWidget {
-  const ChatBotPage({super.key});
+class ChatBotPage extends StatefulWidget {
+  ChatBotPage({super.key});
+
+  @override
+  State<ChatBotPage> createState() => _ChatBotPageState();
+}
+
+class _ChatBotPageState extends State<ChatBotPage> {
+  List messages = [
+    {'message': 'Hello', 'type': 'user'},
+    {'message': 'How can i help you', 'type': 'assistant'},
+    {'message': 'Give me information about you', 'type': 'user'},
+    {'message': 'I am a helful assistant', 'type': 'assistant'},
+  ];
+
+  TextEditingController queryController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chat Bot page"),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+          "Chat Bot page",
+           style: TextStyle(
+              color: Theme.of(context).indicatorColor
+        ),
+        ),
       ),
-      body: Center(
-        child: Text("Chat Bot page"),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        messages[index]['message'],
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                    ),
+                    Divider(
+                      height: 1,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                );
+            }),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: queryController,
+                    // obscureText: true,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(
+                          Icons.visibility
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: (){
+                    String query = queryController.text;
+                    setState(() {
+                      messages.add({"message" : query, "type" : "user"});
+                    });
+
+                  },
+                  icon: Icon(Icons.send,),
+                ),
+              ],
+            ),
+          ),
+
+        ],
       ),
     );
   }
 }
+
