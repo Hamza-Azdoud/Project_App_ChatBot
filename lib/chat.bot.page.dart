1f855c028807afb2acf92c870cf4ddf1676bf11b
Hamza-Azdoud <h.azdoud@etu.enset-media.ac.ma>
2024-07-12 13:46:52 +0100

    first commit

---
+++ lib/chat.bot.page.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+//
+// class ChatBotPage extends StatelessWidget {
+//   const ChatBotPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Chat Bot page"),
+//       ),
+//       body: Center(
+//         child: Text("Chat Bot page"),
+//       ),
+//     );
+//   }
+// }
+
---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:app_chat_bot/chat.bot.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          indicatorColor: Colors.white,
+          primaryColor: Colors.teal,
+      ),
+      routes: {
+        "/chat" :(context)=> ChatBotPage(),
+      },
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // drawer: Drawer(
+      //   child: Column(
+      //     children: [
+      //       DrawerHeader(
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           children: [
+      //             CircleAvatar(
+      //               backgroundImage: AssetImage("images/profile.jpg"),
+      //               radius: 50,
+      //             ),
+      //             CircleAvatar(
+      //               backgroundImage: AssetImage("images/profile.jpg"),
+      //               radius: 50,
+      //             ),
+      //           ],
+      //         ),
+      //           decoration: BoxDecoration(
+      //             gradient: LinearGradient(
+      //                 colors: [
+      //                   Colors.white,
+      //                   Theme.of(context).primaryColor
+      //                 ],
+      //             ),
+      //           )
+      //       )
+      //     ],
+      //   ),
+      // ),
+      appBar: AppBar(
+        title: Text(
+          "Login Page",
+          style: TextStyle(color: Theme.of(context).indicatorColor),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: 400,
+          width: 400 ,
+          child: Card(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(image: AssetImage("images/ENSET Mohammedia.jpg")),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  
+                  TextFormField(
+                    controller: loginController,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(
+                          Icons.lock
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  
+                  SizedBox(
+                    height: 20,
+                  ),
+                  
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(
+                          Icons.visibility
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
+                      onPressed: (){
+                        // String username = loginController.text;
+                        // String password = loginController.text;
+                        // if(username=="admin"&& password=="1234"){
+                        //   Navigator.pushNamed(context, "/chat");
+                        //}
+                      },
+                      child: Text(
+                          "Log In",
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                      ),
+
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
